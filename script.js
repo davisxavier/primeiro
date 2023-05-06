a6dff266f52bcac5eca4a1119c1193b345cecc7d
Davis Xavier <davis.xavier@gmail.com>
2023-05-06 00:21:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem') 
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 19
     msg.innerHTML =`Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //bom dia
