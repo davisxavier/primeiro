f7a8f6a42572e6e29ff9fd6eaf986f7d77a2be10
Davis Xavier <davis.xavier@gmail.com>
2023-05-07 16:57:40 -0300

    inclusão do curriculo sem link

---
+++ curscr.js
@@ -0,0 +1,21 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem') 
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 19
+    msg.innerHTML =`Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src='manha.png'
+        document.main.style.background = 'rgb(235, 81, 43)'  
+    } else if (hora >=12 && hora < 18){
+        //boa tarde
+        img.src = 'tarde.png'
+        document.main.style.background = 'green'
+    } else {
+        //boa noite
+        img.src = 'noite.png'
+        document.main.style.background = 'black'
+    }
+}
