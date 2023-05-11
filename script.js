d191e660b6cd086ffd76c1fcf91bf5beebdcda4b
Davis Xavier <davis.xavier@gmail.com>
2023-05-11 19:37:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,17 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     var min = data.getMinutes()
-    //var hora = 2
-    msg.innerHTML =`Agora são ${hora}:${min}h.`
+    msg.innerHTML =`Agora são ${hora} Hora(s) e ${min} minutos.`
     if (hora >= 0 && hora < 5) {
-        //bom dia
         img.src='madrugada.png'
         document.main.style.background = 'rgb(235, 81, 43)'  
     } else if (hora >=5 && hora < 12){
-        //boa tarde
         img.src = 'manha.png'
         document.main.style.background = 'green'
     } else if (hora >=12 && hora < 18){
-        //boa tarde
         img.src = 'tarde.png'
         document.main.style.background = 'green'
     } else {
-        //boa noite
         img.src = 'noite.png'
         document.main.style.background = 'black'
     }
