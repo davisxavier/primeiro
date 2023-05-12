a9f5c3ad4d8c9c93e659ebafd25363f8ebda47e2
Davis Xavier <davis.xavier@gmail.com>
2023-05-12 16:18:31 -0300

    Update curscr.js

--- curscr.js
+++ curscr.js
@@ -1,8 +1,6 @@
 var sma = window.document.getElementById('dnavcur')
 var url = "https://docs.google.com/forms/d/e/1FAIpQLSes2leFtJYoQl27JLNvGJuZ1pTRk1_Ff8iAMpWxWCh6vO7lPA/viewform?usp=sf_link"
 sma.addEventListener('Sobre mim', divcontato)
-//sma.addEventListener('mouseenter', mentcontato)
-//sma.addEventListener('mouseout',moutcontato)
 function divcontato() {
 sma.style.background= 'white'
 //sma.innerText = 'Click'
